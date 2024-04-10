75ed947f4190c3c595463a5b805c53dd8a6ed4cc
angel-ayaya <108294233+angel-ayaya@users.noreply.github.com>
2024-04-10 11:00:46 -0700

    Agregar consulta de validación de conexión a la base de datos

--- server/db.js
+++ server/db.js
@@ -8,4 +8,16 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
+// Realizar una consulta simple para validar la conexión
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err.stack);
+    // Aquí podrías lanzar un error o manejar la situación de error como mejor te parezca
+    // Por ejemplo, podrías terminar el proceso si es crítico que tu aplicación se conecte a la DB al inicio
+    process.exit(1);
+  } else {
+    console.log('Conexión a la base de datos establecida exitosamente:', res.rows[0]);
+  }
+});
+
 module.exports = pool;
