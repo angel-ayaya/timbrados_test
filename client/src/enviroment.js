4800cb7c7b02b9e7000c7d71cda7ad5a8d969ea7
angel-ayaya <108294233+angel-ayaya@users.noreply.github.com>
2024-04-10 10:57:42 -0700

    Agrega validación para la variable de entorno REACT_APP_API_URL

--- client/src/enviroment.js
+++ client/src/enviroment.js
@@ -1,5 +1,10 @@
 // src/environment.js
 
+// Lanza un error si REACT_APP_API_URL no está definido
+if (!process.env.REACT_APP_API_URL) {
+  throw new Error("La variable de entorno REACT_APP_API_URL no está definida.");
+}
+
 const apiUrl = process.env.REACT_APP_API_URL;
 // Define otras variables de entorno que necesites aquí
 
