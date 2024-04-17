8afd08e9ae44eb6d62c63ee001af07dbf6e7dd53
angel-ayaya <108294233+angel-ayaya@users.noreply.github.com>
2024-04-17 16:31:28 -0700

    Actualizar script de implementación para generar archivos de producción y eliminar directorio existente si es necesario

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Eliminar el directorio existente
-echo "Eliminando el directorio existente..."
-rm -r /var/www/html/build
+# Ejecutar npm run build para generar los archivos de producción
+echo "Ejecutando npm run build..."
+npm run build
+
+# Verificar si el directorio build existe
+if [ -d "/var/www/html/build" ]; then
+    # Eliminar el directorio existente
+    echo "Eliminando el directorio existente..."
+    rm -r /var/www/html/build
+fi
 
 # Copiar el nuevo directorio
 echo "Copiando el nuevo directorio..."
