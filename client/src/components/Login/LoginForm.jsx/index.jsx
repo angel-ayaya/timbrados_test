b19e4dda5dc665ea740a3d47dcd346fef00f7ce4
angel-ayaya <108294233+angel-ayaya@users.noreply.github.com>
2024-04-17 16:03:48 -0700

    Agregada importación de apiUrl en LoginForm.jsx

--- client/src/components/Login/LoginForm.jsx/index.jsx
+++ client/src/components/Login/LoginForm.jsx/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { apiUrl } from "../../enviroment";
 
 function LoginForm() {
   const [username, setUsername] = useState('');
@@ -9,7 +10,7 @@ function LoginForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:3001/api/auth/login', {
+      const response = await fetch(`${apiUrl}api/auth/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
