946ed7f7112dd2fb03e5043019d38798d9ad71d1
angel-ayaya <108294233+angel-ayaya@users.noreply.github.com>
2024-04-24 13:39:32 -0700

    Actualizar consulta SQL para utilizar la tabla "timbrado_new" en lugar de "timbrado2017"

--- server/models/nominasModel.js
+++ server/models/nominasModel.js
@@ -6,9 +6,11 @@ const pool = require("./../db");
  * año de la nomina : anio
  * tipo de nomina : nomina
  */
+const table = "timbrado_new";
+
 const getNomina = async (numempleado, anio, nomina) => {
   const queryBody = `modulo, anio, ciclo, perext, folio, mespago, foliofiscal, totalimpret, totgravado, totexento, totpercep, totdeducc, subtotal, nombre, numempleado, rfcre, lcurp, nomina`;
-  const query = `SELECT ${queryBody} FROM timbrado2017 WHERE numempleado = $1 AND anio = $2 AND nomina = $3 order by CICLO,mespago;`;
+  const query = `SELECT ${queryBody} FROM ${table}  WHERE numempleado = $1 AND anio = $2 AND nomina = $3 order by CICLO,mespago;`;
 
   try {
     const result = await pool.query(query, [numempleado, anio, nomina]);
@@ -23,16 +25,16 @@ const getNomina = async (numempleado, anio, nomina) => {
 const subirNuevosTimbrados = async (registros) => {
   for (const registro of registros) {
     await pool.query(
-      `INSERT INTO timbrado2017 (repositorio, modulo, nomina, anio, ciclo, perext, numempleado, statcancela, totpercep, totdeducc, totgravado, totexento, totsueldo, tototrosded, totisr, folio, aniopago, mespago, totalimpret, basegrabada, subtotal, nombranum, nombre, key, clavenombramiento, pactimbrado, foliofiscal, rfcre, lcurp)
+      `INSERT INTO ${table} (repositorio, modulo, nomina, anio, ciclo, perext, numempleado, statcancela, totpercep, totdeducc, totgravado, totexento, totsueldo, tototrosded, totisr, folio, aniopago, mespago, totalimpret, basegrabada, subtotal, nombranum, nombre, key, clavenombramiento, pactimbrado, foliofiscal, rfcre, lcurp)
        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29)`,
       [
-        registro.repositorio, registro.modulo, registro.nomina, registro.anio, registro.ciclo,
-        registro.perext, registro.numempleado, registro.statcancela, registro.totpercep,
-        registro.totdeducc, registro.totgravado, registro.totexento, registro.totsueldo,
-        registro.tototrosded, registro.totisr, registro.folio, registro.aniopago, registro.mespago,
-        registro.totalimpret, registro.basegrabada, registro.subtotal, registro.nombranum, registro.nombre,
-        registro.key, registro.clavenombramiento, registro.pactimbrado, registro.foliofiscal, registro.rfcre,
-        registro.lcurp
+      registro.repositorio, registro.modulo, registro.nomina, registro.anio, registro.ciclo,
+      registro.perext, registro.numempleado, registro.statcancela, registro.totpercep,
+      registro.totdeducc, registro.totgravado, registro.totexento, registro.totsueldo,
+      registro.tototrosded, registro.totisr, registro.folio, registro.aniopago, registro.mespago,
+      registro.totalimpret, registro.basegrabada, registro.subtotal, registro.nombranum, registro.nombre,
+      registro.key, registro.clavenombramiento, registro.pactimbrado, registro.foliofiscal, registro.rfcre,
+      registro.lcurp
       ]
     );
   }
